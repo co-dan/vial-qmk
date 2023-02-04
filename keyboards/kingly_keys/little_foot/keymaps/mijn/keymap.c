c23d25af0729878318174a69dc8b7b7fde2ab40e
Dan Frumin <dan@groupoid.moe>
2023-02-04 15:27:18 +0100

    wut

--- keyboards/kingly_keys/little_foot/keymaps/mijn/keymap.c
+++ keyboards/kingly_keys/little_foot/keymaps/mijn/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [_RAISE] = LAYOUT_split_space_base(
-    KC_ESC,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_MINS, KC_BSPC,
+    KC_ESC,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_VOLD, KC_VOLU,
     KC_1,   KC_2,       KC_3,       KC_4,       KC_5,    KC_6,    KC_7,       KC_8,       KC_9,             KC_0,
     KC_GRV,  KC_MINS, KC_EQL,  KC_BSLS, KC_QUOT, KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT,KC_QUOTE,
     KC_LCBR, KC_RCBR, KC_LPRN, KC_RPRN, KC_COLN, KC_TRNS, KC_TRNS,   KC_TRNS, KC_TRNS, KC_BACKSLASH,
@@ -37,7 +37,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ), 
 
   [_LOWER] = LAYOUT_split_space_base(
-    KC_1,   KC_2,       KC_3,       KC_4,       KC_5,    KC_6,    KC_7,       KC_8,       KC_9,             KC_0,
+    KC_ESC,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_MINS, KC_BSPC,    
     KC_EXLM, KC_AT,   KC_HASH,  KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS,  KC_EQL,
     KC_TILD, KC_UNDS, KC_PLUS, KC_PIPE, KC_DQT,  KC_LEFT, KC_DOWN,  KC_UP, KC_RIGHT, KC_QUOTE,
     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_UNDS, KC_PLUS, KC_LBRC, KC_RBRC, KC_BACKSLASH,
