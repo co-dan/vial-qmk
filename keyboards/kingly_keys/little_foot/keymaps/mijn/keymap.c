15d00d8ae5a2764621db8fdb745056c6600c5ab1
Dan Frumin <dan@groupoid.moe>
2023-03-07 21:47:19 +0100

    update littlefoot cfg

--- keyboards/kingly_keys/little_foot/keymaps/mijn/keymap.c
+++ keyboards/kingly_keys/little_foot/keymaps/mijn/keymap.c
@@ -18,10 +18,16 @@ enum layer_names {
 #define MT_G(X) MT(MOD_LGUI, X)
 #define MT_S(X) MT(MOD_LSFT, X)
 
+const uint16_t PROGMEM test_combo1[] = {MT_C(KC_F), MT_C(KC_J), COMBO_END};
+combo_t key_combos[COMBO_COUNT] = {
+    COMBO(test_combo1, CAPS_WORD)
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_BASE] = LAYOUT_split_space_base(
     KC_1,   KC_2,       KC_3,       KC_4,       KC_5,    KC_6,    KC_7,       KC_8,       KC_9,             KC_0,
+    //    KC_1,   KC_2,       KC_3,       KC_4,       KC_5,    KC_6,    KC_7,       KC_8,       KC_BSPC,             KC_0,
     KC_Q,   KC_W,       KC_E,       KC_R,       KC_T,    KC_Y,    KC_U,       KC_I,       KC_O,             KC_P,
     KC_A,   MT_G(KC_S), MT_S(KC_D), MT_C(KC_F), KC_G,    KC_H,    MT_C(KC_J), MT_S(KC_K), MT_G(KC_L),  LT(_CONTROL, KC_SCLN),
     KC_Z,   KC_X,       KC_C,       KC_V,       KC_B,    KC_N,    KC_M,       KC_COMM,    KC_DOT,         KC_SLSH,
@@ -47,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_1,   KC_2,       KC_3,       KC_4,       KC_5,    KC_6,    KC_7,       KC_8,       KC_9,             KC_0,
     KC_GRV, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGUP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TAB, KC_TRNS, KC_TRNS, KC_TRNS, KC_PGDN, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, KC_TRNS,
-    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLD, KC_VOLU, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                   KC_TRNS,             KC_TRNS,          KC_TRNS,                KC_TRNS
   ),
   [_ADJUST] = LAYOUT_split_space_base(
@@ -88,3 +94,27 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
 
 
 
+/* // Liatris specific stuff */
+
+/* void keyboard_post_init_user(void) { */
+/*   // Initialize RGB to static black */
+/*   rgblight_enable_noeeprom(); */
+/*   rgblight_sethsv_noeeprom(HSV_BLACK); */
+/*   rgblight_mode_noeeprom(RGBLIGHT_MODE_STATIC_LIGHT); */
+/* } */
+
+/* void housekeeping_task_user(void) { */
+/*   switch (get_highest_layer(layer_state | default_layer_state)) { */
+/*   case 0: */
+/*     rgblight_setrgb_at(RGB_WHITE, 0); */
+/*     break; */
+/*   case 1: */
+/*     rgblight_setrgb_at(10,10,200, 0); */
+/*     break; */
+/*   case 2: */
+/*     rgblight_setrgb_at(10,200,10, 0); */
+/*     break; */
+/*   default: */
+/*     rgblight_setrgb_at(200,10,10, 0); */
+/*   } */
+/* } */
--- keyboards/kingly_keys/little_foot/keymaps/mijn/rules.mk
+++ keyboards/kingly_keys/little_foot/keymaps/mijn/rules.mk
@@ -1,3 +1,11 @@
 AUTO_SHIFT_ENABLE = yes
 WPM_ENABLE      = no
 
+
+CAPS_WORD_ENABLE = yes
+COMBO_ENABLE = yes
+
+# liatris specific stuff
+#RGBLIGHT_ENABLE = yes # Enables QMK's RGB code
+#WS2812_DRIVER = vendor # Use the RP2040's PIO interface
+
