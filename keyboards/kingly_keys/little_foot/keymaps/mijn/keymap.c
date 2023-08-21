65a563be882c743b730a01247823c8589ddec2c3
Dan Frumin <dan@groupoid.moe>
2023-08-21 11:05:04 +0200

    update littlefoot cfg

--- keyboards/kingly_keys/little_foot/keymaps/mijn/keymap.c
+++ keyboards/kingly_keys/little_foot/keymaps/mijn/keymap.c
@@ -26,8 +26,8 @@ combo_t key_combos[COMBO_COUNT] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_BASE] = LAYOUT_split_space_base(
-    /* KC_1,   KC_2,       KC_3,       KC_4,       KC_5,    KC_6,    KC_7,       KC_8,       KC_9,             KC_0, */
-    KC_1,   KC_2,       KC_3,       KC_4,       KC_5,    KC_6,    KC_7,       KC_8,       KC_BSPC,             KC_0,
+    KC_1,   KC_2,       KC_3,       KC_4,       KC_5,    KC_6,    KC_7,       KC_8,       KC_9,             KC_BSPC,
+    /* KC_1,   KC_2,       KC_3,       KC_4,       KC_5,    KC_6,    KC_7,       KC_8,       KC_BSPC,             KC_0, */
     KC_Q,   KC_W,       KC_E,       KC_R,       KC_T,    KC_Y,    KC_U,       KC_I,       KC_O,             KC_P,
     KC_A,   MT_G(KC_S), MT_S(KC_D), MT_C(KC_F), KC_G,    KC_H,    MT_C(KC_J), MT_S(KC_K), MT_G(KC_L),  LT(_CONTROL, KC_SCLN),
     KC_Z,   KC_X,       KC_C,       KC_V,       KC_B,    KC_N,    KC_M,       KC_COMM,    KC_DOT,         KC_SLSH,
