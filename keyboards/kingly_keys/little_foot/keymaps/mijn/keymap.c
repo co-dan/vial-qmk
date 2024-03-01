949f485df5f0d9bf442e31721d1ab7dd24fa8065
Dan Frumin <dan@groupoid.moe>
2024-03-01 21:09:34 +0100

    update??

--- keyboards/kingly_keys/little_foot/keymaps/mijn/keymap.c
+++ keyboards/kingly_keys/little_foot/keymaps/mijn/keymap.c
@@ -20,14 +20,14 @@ enum layer_names {
 
 const uint16_t PROGMEM test_combo1[] = {MT_C(KC_F), MT_C(KC_J), COMBO_END};
 combo_t key_combos[COMBO_COUNT] = {
-    COMBO(test_combo1, CAPS_WORD)
+    COMBO(test_combo1, CW_TOGG)
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_BASE] = LAYOUT_split_space_base(
-    /* KC_1,   KC_2,       KC_3,       KC_4,       KC_5,    KC_6,    KC_7,       KC_8,       KC_9,             KC_BSPC, */
-    KC_1,   KC_2,       KC_3,       KC_4,       KC_5,    KC_6,    KC_7,       KC_8,       KC_9,             KC_0,
+    KC_1,   KC_2,       KC_3,       KC_4,       KC_5,    KC_6,    KC_7,       KC_8,       KC_BSPC,             KC_0,
+    /* KC_1,   KC_2,       KC_3,       KC_4,       KC_5,    KC_6,    KC_7,       KC_8,       KC_9,             KC_0, */
     KC_Q,   KC_W,       KC_E,       KC_R,       KC_T,    KC_Y,    KC_U,       KC_I,       KC_O,             KC_P,
     KC_A,   MT_G(KC_S), MT_S(KC_D), MT_C(KC_F), KC_G,    KC_H,    MT_C(KC_J), MT_S(KC_K), MT_G(KC_L),  LT(_CONTROL, KC_SCLN),
     KC_Z,   KC_X,       KC_C,       KC_V,       KC_B,    KC_N,    KC_M,       KC_COMM,    KC_DOT,         KC_SLSH,
